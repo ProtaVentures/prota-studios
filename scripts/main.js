c36e48f44a76fbf1328d876a7765810291837a00
Will Little <wclittle@gmail.com>
2017-04-13 20:12:35 -0500

    updates

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@ $(function() {
     max: 100,
     step: 1,
     slide: function(event, ui ) {
-      tooltip1.text(ui.value + '%');
+      //tooltip1.text(ui.value);
     },
     change: function(event, ui) {}
   });
@@ -28,17 +28,19 @@ $(function() {
     max: 100,
     step: 25,
     slide: function(event, ui ) {
-      var num1 = ui.value < 100 ? (100 - ui.value) : '';
-      var num2 = ui.value > 0 ? ui.value : '';
+      var num1 = ui.value < 100 ? (100 - ui.value) + "%" : '';
+      var num2 = ui.value > 0 ? ui.value + "%" : '';
       var divider = ui.value < 100 && ui.value > 0 ? ' / ' : '';
-      tooltip2.text(num1 + divider + num2);
+
+      tooltip2.text(num1 + divider + num2 );
     },
     change: function(event, ui) {}
   });
 
   $('#slider1 .ui-slider-handle').append(tooltip1);
-  $('#tooltip1').text('50%');
+  //$('#tooltip1').text('50%');
+  $('#tooltip1').hide();
   $('#slider2 .ui-slider-handle').append(tooltip2);
-  $('#tooltip2').text('50 / 50');
+  $('#tooltip2').text('50% / 50%');
 
 });
